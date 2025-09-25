a053b84ee6336eccd2c4d56765266c6d22255a4b
muribabu18-ai <muribabu18@gmail.com>
2025-09-25 11:36:16 +0300

    Add files via upload

---
+++ Semester 1 units IT.c
@@ -0,0 +1,19 @@
+/*
+ Name : Nemuel Murithi 
+ Reg : CT100/G/26244/25
+ Description: Units for the Semester 
+*/
+#include <stdio.h>
+
+//main function
+int main() {
+    printf("Introduction to Computer Applications\n");
+    printf("Fundamentals of ICT\n");
+    printf("Mathematics for Science \n");
+    printf("Analogue Electronics \n");
+    printf("Computer Systems And Organisation\n");
+    printf("Communication and Literacy Skills\n");
+    printf("Essentials Of Accouning\n");
+    printf("Introduction to Programming and Algorithms\n");
+    return 0;
+}
